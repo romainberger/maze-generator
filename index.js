57fb5236ac6b2f8fa04b5c37457f975bdb26eda3
Romain Berger <romain@romainberger.com>
2013-11-03 23:19:59 +0100

    Just one for now

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function(size, algorithm) {
     , algorithm = algorithm || 'recursiveBacktracking'
     , generator = {}
 
-  // Helpers
+  // Helper
   function shuffle(o){
       for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
       return o;
