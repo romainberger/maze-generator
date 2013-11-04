03241f8ef275b9ce9d6704909c8087d786f7c8df
Romain Berger <romain@romainberger.com>
2013-11-04 18:25:22 +0100

    Code style

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function(size, algorithm) {
   algorithm = algorithm || 'recursiveBacktracking'
 
   // Helper
-  function shuffle(o){
+  function shuffle(o) {
       for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x)
       return o
   }
